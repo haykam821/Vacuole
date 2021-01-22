39b8989187b0f10cbfbbad77c52915cef3ce797b
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-01-22 14:09:53 -0500

    Add a command to list all treasure types

--- src/main/java/io/github/haykam821/vacuole/Main.java
+++ src/main/java/io/github/haykam821/vacuole/Main.java
@@ -1,9 +1,11 @@
 package io.github.haykam821.vacuole;
 
+import io.github.haykam821.vacuole.command.VacuoleCommand;
 import io.github.haykam821.vacuole.game.VacuoleConfig;
 import io.github.haykam821.vacuole.game.VacuoleGame;
 import io.github.haykam821.vacuole.treasure.TreasureTypes;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.util.Identifier;
 import xyz.nucleoid.plasmid.game.GameType;
 
@@ -16,5 +18,8 @@ public class Main implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		TreasureTypes.initialize();
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+			VacuoleCommand.register(dispatcher);
+		});
 	}
 }
---
+++ src/main/java/io/github/haykam821/vacuole/command/VacuoleCommand.java
@@ -0,0 +1,30 @@
+package io.github.haykam821.vacuole.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+
+import io.github.haykam821.vacuole.treasure.TreasureType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+
+public final class VacuoleCommand {
+	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+		dispatcher.register(CommandManager.literal("vacuole")
+			.then(CommandManager.literal("treasure")
+			.then(CommandManager.literal("list")
+			.executes(VacuoleCommand::list))));
+	}
+
+	private static int list(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback(new TranslatableText("command.vacuole.treasure.list.header", TreasureType.REGISTRY.keySet().size()), false);
+
+		for (Identifier id : TreasureType.REGISTRY.keySet()) {
+			TreasureType<?> treasure = TreasureType.REGISTRY.get(id);
+			context.getSource().sendFeedback(new TranslatableText("command.vacuole.treasure.list.entry", treasure.getName()), false);
+		}
+
+		return 0;
+	}
+}
