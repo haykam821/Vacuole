168f8a17479f6eb25eb1910f834e47a2588e1baa
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-10-26 22:22:58 -0400

    Add the Sierpinski carpet treasure

---
+++ src/main/java/io/github/haykam821/vacuole/treasure/SierpinskiCarpetTreasure.java
@@ -0,0 +1,83 @@
+package io.github.haykam821.vacuole.treasure;
+
+import java.util.Random;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.gen.stateprovider.BlockStateProvider;
+import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
+
+public class SierpinskiCarpetTreasure extends Treasure {
+	private static final BlockStateProvider DEFAULT_STATE_PROVIDER = new SimpleBlockStateProvider(Blocks.MOSS_CARPET.getDefaultState());
+
+	public static final Codec<SierpinskiCarpetTreasure> CODEC = RecordCodecBuilder.create(instance -> {
+		return instance.group(
+			Codec.intRange(0, Integer.MAX_VALUE).optionalFieldOf("padding", 0).forGetter(treasure -> treasure.padding),
+			BlockStateProvider.TYPE_CODEC.optionalFieldOf("state_provider", DEFAULT_STATE_PROVIDER).forGetter(treasure -> treasure.stateProvider)
+		).apply(instance, SierpinskiCarpetTreasure::new);
+	});
+
+	private static final Random RANDOM = new Random();
+
+	private final int padding;
+	private final BlockStateProvider stateProvider;
+
+	public SierpinskiCarpetTreasure(int padding, BlockStateProvider stateProvider) {
+		super(TreasureTypes.SIERPINSKI_CARPET);
+
+		this.padding = padding;
+		this.stateProvider = stateProvider;
+	}
+
+	public SierpinskiCarpetTreasure() {
+		this(0, DEFAULT_STATE_PROVIDER);
+	}
+
+	@Override
+	public void build() {
+		super.build();
+
+		int minX = this.canvas.getMin().getX();
+		int maxX = this.canvas.getMax().getX();
+
+		int minZ = this.canvas.getMin().getZ();
+		int maxZ = this.canvas.getMax().getZ();
+		
+		int maxSize = Math.min(maxX - minX - padding * 2, maxZ - minZ - padding * 2);
+		int order = (int) (Math.log(maxSize + 1) / Math.log(3));
+
+		int size = (int) Math.pow(3, order);
+		int paddingX = MathHelper.ceil((maxX - minX - size) / 2d);
+		int paddingZ = MathHelper.ceil((maxZ - minZ - size) / 2d);
+
+		BlockPos.Mutable pos = this.canvas.getMin().mutableCopy().move(Direction.UP);
+
+		for (int x = 0; x < size; x++) {
+			pos.setX(minX + paddingX + x);
+			for (int z = 0; z < size; z++) {
+				pos.setZ(minZ + paddingZ + z);
+				if (this.isBlock(x, z)) {
+					this.canvas.setBlockState(pos, this.stateProvider.getBlockState(RANDOM, pos));
+				}
+			}
+		}
+	}
+	
+	private boolean isBlock(int x, int z) {
+		while (x != 0 && z != 0) {
+			if (x % 3 == 1 && z % 3 == 1) {
+				return false;
+			}
+
+			x /= 3;
+			z /= 3;
+		}
+
+		return true;
+	}
+}
--- src/main/java/io/github/haykam821/vacuole/treasure/TreasureTypes.java
+++ src/main/java/io/github/haykam821/vacuole/treasure/TreasureTypes.java
@@ -20,6 +20,7 @@ public final class TreasureTypes {
 	public static final TreasureType<StaircaseTreasure> STAIRCASE = register("staircase", StaircaseTreasure.CODEC, StaircaseTreasure::new, Items.BRICK_STAIRS);
 	public static final TreasureType<ConveyorTreasure> CONVEYOR = register("conveyor", ConveyorTreasure.CODEC, ConveyorTreasure::new, Items.MAGENTA_GLAZED_TERRACOTTA);
 	public static final TreasureType<CenterTreasure> CENTER = register("center", CenterTreasure.CODEC, CenterTreasure::new, Items.END_ROD);
+	public static final TreasureType<SierpinskiCarpetTreasure> SIERPINSKI_CARPET = register("sierpinski_carpet", SierpinskiCarpetTreasure.CODEC, SierpinskiCarpetTreasure::new, Items.MOSS_CARPET);
 
 	private static <T extends Treasure> TreasureType<T> register(String path, Codec<T> codec, Supplier<T> creator, Item icon) {
 		Identifier id = new Identifier(Main.MOD_ID, path);
